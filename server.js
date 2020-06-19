218e2bb293eb25efe0800b73deb2992a393b9668
BrandonKutivan <bskutivan@gmail.com>
2020-06-18 18:59:36 -0700

    added code for server file and created connection

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Shhhhhh',
+    cookie: { maxAge: 75000 },
+    resave: true,
+    rolling: true,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+const app = express();
+const PORT = process.env.PORT || 3004;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening at port ${PORT}`));
+});
